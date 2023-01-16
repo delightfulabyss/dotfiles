f2bd42d47bad329614d81ef6cc422059378b8a78
Matthew Watman <mattwatman@gmail.com>
2023-01-15 21:56:33 -0500

    Update .config/nvim/lua/config/lsp.lua

--- private_dot_config/nvim/lua/config/lsp.lua
+++ private_dot_config/nvim/lua/config/lsp.lua
@@ -116,7 +116,7 @@ require("lspconfig").tailwindcss.setup({})
 vim.diagnostic.config({
 	underline = true,
 	signs = true,
-	virtual_text = false,
+	virtual_text = true,
 	float = {
 		show_header = true,
 		source = "always",
