9f480a911687cc9d34f4d8d982f16472e710e6d6
Matthew Watman <mattwatman@gmail.com>
2023-01-21 11:39:12 -0500

    Update .config/nvim/lua/plugins.lua

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -129,7 +129,7 @@ lazy.setup({
 	{ "numToStr/Navigator.nvim" },
 	{ "windwp/nvim-autopairs" },
 	{ "folke/which-key.nvim" },
-	{ "akinsho/bufferline.nvim", tag = "v3.*", dependencies = "nvim-tree/nvim-web-devicons" },
+	{ "akinsho/bufferline.nvim", tag = "latest", dependencies = "nvim-tree/nvim-web-devicons" },
 	{ "rafamadriz/friendly-snippets" },
 	{ "mfussenegger/nvim-dap" },
 	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
