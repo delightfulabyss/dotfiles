f6b85f088432fd28bf372e69b880c829ae5b9468
Matthew Watman <mattwatman@gmail.com>
2023-01-21 12:24:35 -0500

    Update .config/nvim/lua/plugins.lua

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -19,6 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 
 local lazy = require("lazy")
 lazy.setup({
+	--Regularly loaded plugins
 	{ -- LSP Configuration & Plugins
 		"neovim/nvim-lspconfig",
 		dependencies = {
@@ -56,96 +57,31 @@ lazy.setup({
 
 	-- Git related plugins
 	"lewis6991/gitsigns.nvim",
-	{
-		"pwntester/octo.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-			"nvim-tree/nvim-web-devicons",
-		},
-	},
 	"nvim-lualine/lualine.nvim", -- Fancier statusline
 	"lukas-reineke/indent-blankline.nvim", -- Add indentation guides even on blank lines
-	"numToStr/Comment.nvim", -- "gc" to comment visual regions/lines
-
-	-- Fuzzy Finder (files, lsp, etc)
-	{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-
-	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = vim.fn.executable("make") == 1 },
-
-	-- Leap.nvim efficient naviation
-	{
-		"ggandor/leap.nvim",
-		event = "BufRead",
-		dependencies = "tpope/vim-repeat",
-		config = function()
-			require("leap").add_default_mappings()
-		end,
-	},
-
-	-- Markdown
-	{ "jakewvincent/mkdnflow.nvim" },
 
 	-- Registers
 	{ "tversteeg/registers.nvim" },
 
-	-- Booleans
-	{ "https://github.com/nat-418/boole.nvim" },
-
-	-- Color picker
-	{ "uga-rosa/ccc.nvim" },
-
-	{
-		"narutoxy/silicon.lua",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-
-	{ "hlucco/nvim-eswpoch" },
-	{ "folke/zen-mode.nvim", cmd = "ZenMode" },
 	{ "jbyuki/one-small-step-for-vimkind" },
-	{ "nanotee/luv-vimdocs" },
-	{ "xorid/asciitree.nvim" },
 	{
 		"folke/todo-comments.nvim",
 		dependencies = "nvim-lua/plenary.nvim",
 	},
-	{
-		"jackMort/ChatGPT.nvim",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"nvim-lua/plenary.nvim",
-		},
-	},
-	{ "stevearc/overseer.nvim" },
 	{ "Weissle/persistent-breakpoints.nvim" },
 	{ "folke/tokyonight.nvim" },
 	{ "brenoprata10/nvim-highlight-colors" },
-	{
-		"goolord/alpha-nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
-	{ "edluffy/specs.nvim" },
 	{ "numToStr/Navigator.nvim" },
 	{ "windwp/nvim-autopairs" },
 	{ "folke/which-key.nvim" },
 	{ "rafamadriz/friendly-snippets" },
 	{ "mfussenegger/nvim-dap" },
-	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
 	{ "theHamsta/nvim-dap-virtual-text" },
 	{ "nvim-telescope/telescope-dap.nvim" },
 	{ "jayp0521/mason-nvim-dap.nvim", dependencies = { "mfussenegger/nvim-dap", "williamboman/mason.nvim" } },
 	{ "mxsdev/nvim-dap-vscode-js", dependencies = { "mfussenegger/nvim-dap" } },
 	{ "RRethy/vim-illuminate" },
-	{
-		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
 	"jose-elias-alvarez/null-ls.nvim",
-	{ "barrett-ruth/telescope-http.nvim" },
-	{ "chip/telescope-software-licenses.nvim" },
-	{ "nvim-telescope/telescope-media-files.nvim" },
-	{ "nvim-telescope/telescope-symbols.nvim" },
 	{ "mrjones2014/nvim-ts-rainbow" },
 	{ "chentoast/marks.nvim" },
 	{
@@ -156,10 +92,69 @@ lazy.setup({
 			"antoinemadec/FixCursorHold.nvim",
 		},
 	},
-	{ "stevearc/oil.nvim" },
 	{ "psliwka/vim-smoothie" },
-	{ "tpope/vim-fugitive" },
 	{ "Exafunction/codeium.vim" },
+
+	--Lazily-loaded plugins
+	{ "folke/zen-mode.nvim", cmd = "ZenMode" },
+	{ "tpope/vim-fugitive" },
+	{ "stevearc/oil.nvim" },
+	{ "barrett-ruth/telescope-http.nvim" },
+	{ "chip/telescope-software-licenses.nvim" },
+	{ "nvim-telescope/telescope-media-files.nvim" },
+	{ "nvim-telescope/telescope-symbols.nvim" },
+	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{ "stevearc/overseer.nvim" },
+	{
+		"jackMort/ChatGPT.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{ "xorid/asciitree.nvim" },
+	{ "hlucco/nvim-eswpoch" },
+	{
+		"narutoxy/silicon.lua",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	-- Color picker
+	{ "uga-rosa/ccc.nvim" },
+	-- Booleans
+	{ "https://github.com/nat-418/boole.nvim" },
+	-- Markdown
+	{ "jakewvincent/mkdnflow.nvim" },
+	-- Leap.nvim efficient naviation
+	{
+		"ggandor/leap.nvim",
+		event = "BufRead",
+		dependencies = "tpope/vim-repeat",
+		config = function()
+			require("leap").add_default_mappings()
+		end,
+	},
+	-- Fuzzy Finder (files, lsp, etc)
+	{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+
+	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = vim.fn.executable("make") == 1 },
+	{
+		"pwntester/octo.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{ "numToStr/Comment.nvim" }, -- "gc" to comment visual regions/lines
 })
 -- { "Tastyep/structlog.nvim" })
 -- {
