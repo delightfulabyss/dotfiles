1c56d1f540d573179eeb47760dbb2b917878c452
Matthew Watman <mattwatman@gmail.com>
2023-02-01 11:02:38 -0500

    Update .config/nvim/lua/plugins/trailblazer.lua

--- private_dot_config/nvim/lua/plugins/trailblazer.lua
+++ private_dot_config/nvim/lua/plugins/trailblazer.lua
@@ -20,7 +20,7 @@ return {
 			verbose_trail_mark_select = true, -- print current mode notification on mode change
 			mark_symbol = "•", --  will only be used if trail_mark_symbol_line_indicators_enabled = true
 			newest_mark_symbol = "", -- disable this mark symbol by setting its value to ""
-			cursor_mark_symbol = "", -- disable this mark symbol by setting its value to ""
+			cursor_mark_symbol = "󰇀", -- disable this mark symbol by setting its value to ""
 			next_mark_symbol = "", -- disable this mark symbol by setting its value to ""
 			previous_mark_symbol = "", -- disable this mark symbol by setting its value to ""
 			multiple_mark_symbol_counters_enabled = true,
