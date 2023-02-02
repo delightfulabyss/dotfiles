0edba2af4c665ed61992084db19920e75d037d20
Matthew Watman <mattwatman@gmail.com>
2023-02-01 20:49:52 -0500

    Update .config/nvim/lua/plugins/trailblazer.lua

--- private_dot_config/nvim/lua/plugins/trailblazer.lua
+++ private_dot_config/nvim/lua/plugins/trailblazer.lua
@@ -1,4 +1,5 @@
-local theme = require("tokyonight.colors")
+cal
+theme = require("tokyonight.colors")
 
 return {
 	"LeonHeidelbach/trailblazer.nvim",
@@ -6,98 +7,100 @@ return {
 	config = function()
 		require("trailblazer").setup({
 			lang = "en",
-			available_trail_mark_modes = {
-				"global_chron",
-				"global_buf_line_sorted",
-				"global_chron_buf_line_sorted",
-				"global_chron_buf_switch_group_chron",
-				"global_chron_buf_switch_group_line_sorted",
-				"buffer_local_chron",
-				"buffer_local_line_sorted",
-			},
-			current_trail_mark_mode = "global_chron",
-			current_trail_mark_list_type = "quickfix", -- currently only quickfix lists are supported
-			verbose_trail_mark_select = true, -- print current mode notification on mode change
-			mark_symbol = "", --  will only be used if trail_mark_symbol_line_indicators_enabled = true
-			newest_mark_symbol = "", -- disable this mark symbol by setting its value to ""
-			cursor_mark_symbol = "󰇀", -- disable this mark symbol by setting its value to ""
-			next_mark_symbol = "", -- disable this mark symbol by setting its value to ""
-			previous_mark_symbol = "", -- disable this mark symbol by setting its value to ""
-			multiple_mark_symbol_counters_enabled = true,
-			number_line_color_enabled = true,
-			trail_mark_in_text_highlights_enabled = true,
-			trail_mark_symbol_line_indicators_enabled = true, -- show indicators for all trail marks in symbol column
-			symbol_line_enabled = true,
-			default_trail_mark_stacks = {
-				"default", -- , "stack_2", ...
-			},
-			available_trail_mark_stack_sort_modes = {
-				"alpha_asc", -- alphabetical ascending
-				"alpha_dsc", -- alphabetical descending
-				"chron_asc", -- chronological ascending
-				"chron_dsc", -- chronological descending
-			},
-			current_trail_mark_stack_sort_mode = "alpha_asc",
-			hl_groups = {
-				TrailBlazerTrailMark = {
-					guifg = "White",
-					guibg = "none",
-					gui = "bold",
+			trail_options = {
+				available_trail_mark_modes = {
+					"global_chron",
+					"global_buf_line_sorted",
+					"global_chron_buf_line_sorted",
+					"global_chron_buf_switch_group_chron",
+					"global_chron_buf_switch_group_line_sorted",
+					"buffer_local_chron",
+					"buffer_local_line_sorted",
 				},
-				TrailBlazerTrailMarkNext = {
-					guifg = "Green",
-					guibg = "none",
-					gui = "bold",
+				current_trail_mark_mode = "global_chron",
+				current_trail_mark_list_type = "quickfix", -- currently only quickfix lists are supported
+				verbose_trail_mark_select = true, -- print current mode notification on mode change
+				mark_symbol = ".", --  will only be used if trail_mark_symbol_line_indicators_enabled = true
+				newest_mark_symbol = "", -- disable this mark symbol by setting its value to ""
+				cursor_mark_symbol = "󰇀", -- disable this mark symbol by setting its value to ""
+				next_mark_symbol = "", -- disable this mark symbol by setting its value to ""
+				previous_mark_symbol = "", -- disable this mark symbol by setting its value to ""
+				multiple_mark_symbol_counters_enabled = true,
+				number_line_color_enabled = true,
+				trail_mark_in_text_highlights_enabled = true,
+				trail_mark_symbol_line_indicators_enabled = true, -- show indicators for all trail marks in symbol column
+				symbol_line_enabled = true,
+				default_trail_mark_stacks = {
+					"default", -- , "stack_2", ...
 				},
-				TrailBlazerTrailMarkPrevious = {
-					guifg = "Red",
-					guibg = "none",
-					gui = "bold",
+				available_trail_mark_stack_sort_modes = {
+					"alpha_asc", -- alphabetical ascending
+					"alpha_dsc", -- alphabetical descending
+					"chron_asc", -- chronological ascending
+					"chron_dsc", -- chronological descending
 				},
-				TrailBlazerTrailMarkCursor = {
-					guifg = "Black",
-					guibg = "Orange",
-					gui = "bold",
-				},
-				TrailBlazerTrailMarkNewest = {
-					guifg = "Black",
-					guibg = "LightBlue",
-					gui = "bold",
-				},
-				TrailBlazerTrailMarkGlobalChron = {
-					guifg = "Black",
-					guibg = "Red",
-					gui = "bold",
-				},
-				TrailBlazerTrailMarkGlobalBufLineSorted = {
-					guifg = "Black",
-					guibg = "LightRed",
-					gui = "bold",
-				},
-				TrailBlazerTrailMarkGlobalChronBufLineSorted = {
-					guifg = "Black",
-					guibg = "Olive",
-					gui = "bold",
-				},
-				TrailBlazerTrailMarkGlobalChronBufSwitchGroupChron = {
-					guifg = "Black",
-					guibg = "VioletRed",
-					gui = "bold",
-				},
-				TrailBlazerTrailMarkGlobalChronBufSwitchGroupLineSorted = {
-					guifg = "Black",
-					guibg = "MediumSpringGreen",
-					gui = "bold",
-				},
-				TrailBlazerTrailMarkBufferLocalChron = {
-					guifg = "Black",
-					guibg = "Green",
-					gui = "bold",
-				},
-				TrailBlazerTrailMarkBufferLocalLineSorted = {
-					guifg = "Black",
-					guibg = "LightGreen",
-					gui = "bold",
+				current_trail_mark_stack_sort_mode = "alpha_asc",
+				hl_groups = {
+					TrailBlazerTrailMark = {
+						guifg = "White",
+						guibg = "none",
+						gui = "bold",
+					},
+					TrailBlazerTrailMarkNext = {
+						guifg = "Green",
+						guibg = "none",
+						gui = "bold",
+					},
+					TrailBlazerTrailMarkPrevious = {
+						guifg = "Red",
+						guibg = "none",
+						gui = "bold",
+					},
+					TrailBlazerTrailMarkCursor = {
+						guifg = "Black",
+						guibg = "Orange",
+						gui = "bold",
+					},
+					TrailBlazerTrailMarkNewest = {
+						guifg = "Black",
+						guibg = "LightBlue",
+						gui = "bold",
+					},
+					TrailBlazerTrailMarkGlobalChron = {
+						guifg = "Black",
+						guibg = "Red",
+						gui = "bold",
+					},
+					TrailBlazerTrailMarkGlobalBufLineSorted = {
+						guifg = "Black",
+						guibg = "LightRed",
+						gui = "bold",
+					},
+					TrailBlazerTrailMarkGlobalChronBufLineSorted = {
+						guifg = "Black",
+						guibg = "Olive",
+						gui = "bold",
+					},
+					TrailBlazerTrailMarkGlobalChronBufSwitchGroupChron = {
+						guifg = "Black",
+						guibg = "VioletRed",
+						gui = "bold",
+					},
+					TrailBlazerTrailMarkGlobalChronBufSwitchGroupLineSorted = {
+						guifg = "Black",
+						guibg = "MediumSpringGreen",
+						gui = "bold",
+					},
+					TrailBlazerTrailMarkBufferLocalChron = {
+						guifg = "Black",
+						guibg = "Green",
+						gui = "bold",
+					},
+					TrailBlazerTrailMarkBufferLocalLineSorted = {
+						guifg = "Black",
+						guibg = "LightGreen",
+						gui = "bold",
+					},
 				},
 			},
 		})
