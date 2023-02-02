a8ad36b1364a7efc3f391c657736bcca23429ed4
Matthew Watman <mattwatman@gmail.com>
2023-02-02 15:16:18 -0500

    Update .config/nvim/lua/plugins/trouble.lua

--- private_dot_config/nvim/lua/plugins/trouble.lua
+++ private_dot_config/nvim/lua/plugins/trouble.lua
@@ -1,7 +1,7 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	cmd = { "Trouble", "TroubleToggle", "TroubleRefresh", "TroubleClose" },
+	event = "BufEnter",
 	config = function()
 		require("trouble").setup({})
 	end,
