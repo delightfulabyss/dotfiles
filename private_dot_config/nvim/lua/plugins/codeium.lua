9e42e86ef9e55f283c736c5e8b44d0ed31f9db54
Matthew Watman <mattwatman@gmail.com>
2023-02-07 12:22:58 -0500

    Update .config/nvim/lua/plugins/codeium.lua

--- private_dot_config/nvim/lua/plugins/codeium.lua
+++ private_dot_config/nvim/lua/plugins/codeium.lua
@@ -2,7 +2,7 @@ return {
 	"Exafunction/codeium.vim",
 	config = function()
 		-- Change '<C-g>' here to any keycode you like.
-		vim.keymap.set("i", "<C-g>", function()
+		vim.keymap.set("i", "<c-g>", function()
 			return vim.fn["codeium#Accept"]()
 		end, { expr = true })
 		vim.keymap.set("i", "<c-;>", function()
