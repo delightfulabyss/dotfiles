9069e58b99948200fef5d0441c11c3c6cdb38237
Matthew Watman <mattwatman@gmail.com>
2023-02-06 21:14:34 -0500

    Update .config/nvim/lua/plugins/codeium.lua

--- private_dot_config/nvim/lua/plugins/codeium.lua
+++ private_dot_config/nvim/lua/plugins/codeium.lua
@@ -2,18 +2,17 @@ return {
 	"Exafunction/codeium.vim",
 	event = "BufEnter",
 	config = function()
-		-- Change '<C-g>' here to any keycode you like.
-		vim.keymap.set("i", "<C-g>", function()
-			vim.fn["codeium#Accept"]()
+		vim.keymap.set("i", "<C-m>", function()
+			return vim.fn["codeium#Accept"]()
 		end, { expr = true })
 		vim.keymap.set("i", "<C-;>", function()
-			vim.fn["codeium#CycleCompletions"](1)
+			return vim.fn["codeium#CycleCompletions"](1)
 		end, { expr = true })
 		vim.keymap.set("i", "<C-,>", function()
-			vim.fn["codeium#CycleCompletions"](-1)
+			return vim.fn["codeium#CycleCompletions"](-1)
 		end, { expr = true })
 		vim.keymap.set("i", "<C-x>", function()
-			vim.fn["codeium#Clear"]()
+			return vim.fn["codeium#Clear"]()
 		end, { expr = true })
 	end,
 }
