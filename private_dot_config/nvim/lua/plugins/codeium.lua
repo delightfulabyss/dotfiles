8689f3939ece2068fd04ab18feb1895279ffd95d
Matthew Watman <mattwatman@gmail.com>
2023-02-07 12:21:32 -0500

    Update .config/nvim/lua/plugins/codeium.lua Update .config/nvim/lua/plugins/lsp.lua

--- private_dot_config/nvim/lua/plugins/codeium.lua
+++ private_dot_config/nvim/lua/plugins/codeium.lua
@@ -1,12 +1,18 @@
 return {
-	"jcdickinson/codeium.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"hrsh7th/nvim-cmp",
-	},
-	event = "BufEnter",
+	"Exafunction/codeium.vim",
 	config = function()
-		require("codeium").setup({})
+		-- Change '<C-g>' here to any keycode you like.
+		vim.keymap.set("i", "<C-g>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true })
+		vim.keymap.set("i", "<c-;>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true })
+		vim.keymap.set("i", "<c-,>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true })
+		vim.keymap.set("i", "<c-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true })
 	end,
 }
