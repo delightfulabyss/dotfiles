6a007cd7ce412b39b6f9f31a31c2a5d12e2860e6
Matthew Watman <mattwatman@gmail.com>
2023-02-09 12:29:40 -0500

    Update .config/nvim/lua/plugins/lsp.lua

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -68,8 +68,7 @@ return {
 					require("mason").setup()
 					-- Enable the following language servers
 					-- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-					local servers =
-					{ "cssls", "rust_analyzer", "solidity", "tsserver", "tailwindcss", "sumneko_lua", "emmet_ls", "ltex-ls" }
+					local servers = { "cssls", "rust_analyzer", "solidity", "tsserver", "tailwindcss", "sumneko_lua", "emmet_ls" }
 
 					-- Ensure the servers above are installed
 					require("mason-lspconfig").setup({
