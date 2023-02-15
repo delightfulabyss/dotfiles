8bcaa11a19982fff0d6633da36a18171e1515af8
Matthew Watman <mattwatman@gmail.com>
2023-02-15 13:09:18 -0500

    Update .config/nvim/lua/plugins/codeium.lua

--- private_dot_config/nvim/lua/plugins/codeium.lua
+++ private_dot_config/nvim/lua/plugins/codeium.lua
@@ -1,9 +1,3 @@
 return {
-				"Exafunction/codeium.vim",
-				config = function()
-					vim.keymap.set("i", "<C-g>", function()
-						return vim.fn["codeium#Accept"]()
-					end, { expr = true })
-					vim
-				end,
+	"Exafunction/codeium.vim",
 }
